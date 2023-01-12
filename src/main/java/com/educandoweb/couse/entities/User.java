ea7a53f58d5ffe57d84ddd62bd10d2efb5960845
VLV01 <vinicius.linsvasco@hotmail.com>
2023-01-12 17:00:35 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/couse/entities/User.java
+++ src/main/java/com/educandoweb/couse/entities/User.java
@@ -1,11 +1,16 @@
 package com.educandoweb.couse.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    @Id//Chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Auto-incremental
     private Long id;
     private String name;
     private String email;
