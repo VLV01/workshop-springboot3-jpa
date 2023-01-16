de62a5efa32889b0a5140fb324aa4e0070ef4bb2
VLV01 <vinicius.linsvasco@hotmail.com>
2023-01-16 17:03:58 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/couse/entities/Order.java
+++ src/main/java/com/educandoweb/couse/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.couse.entities;
 
+import com.educandoweb.couse.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -16,6 +17,7 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
 
 
     @ManyToOne
@@ -24,9 +26,10 @@ public class Order implements Serializable {
 
     public Order() {
     }
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
     public Long getId() {
@@ -43,6 +46,15 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
